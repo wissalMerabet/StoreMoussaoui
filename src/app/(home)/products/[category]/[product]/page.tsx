3e62f6da11c2751d27bc5bed07d62507ccf7a21e
wissal <wissalmerabet5@gmail.com>
2025-07-13 02:09:55 +0200

    update

--- src/app/(home)/products/[category]/[product]/page.tsx
+++ src/app/(home)/products/[category]/[product]/page.tsx
@@ -2,23 +2,20 @@ import { getProductById } from "../../actions";
 import ProductDetail from "@/components/layout/ProductDetail";
 
 interface PageProps {
-  params: { category: string; product: string };
+  params: { product: string };
 }
-
 
 function extractIdFromSlug(slug: string): string | null {
   const match = slug.match(/-(\d+)$/);
   return match ? match[1] : null;
 }
 
-export default async function Page({ params }: PageProps) {
-  const { product } = await params;
+export default async function Page(props: PageProps) {
+  const product = props?.params?.product;
 
   const id = extractIdFromSlug(product);
   if (!id) return null;
 
   const productData = await getProductById(id);
-  
-
   return <ProductDetail product={productData} />;
 }
